140469ce8b859ad6e711fc99d41e1694f4f83bad
MEHDIMYADI <mehdimyadi@gmail.com>
2025-07-04 10:05:07 +0330

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,197 @@
+﻿using System.Windows.Forms;
+
+namespace HA6400AdminUnlocker
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.webView = new Microsoft.Web.WebView2.WinForms.WebView2();
+            this.headerPanel = new System.Windows.Forms.Panel();
+            this.progressRing = new System.Windows.Forms.ProgressBar();
+            this.btnUnlock = new System.Windows.Forms.Button();
+            this.btnRefresh = new System.Windows.Forms.Button();
+            this.btnGithub = new System.Windows.Forms.Button();
+            this.titleLabel = new System.Windows.Forms.Label();
+            this.chkAutoUnlock = new System.Windows.Forms.CheckBox();
+            this.infoMessageText = new System.Windows.Forms.Label();
+            this.infoMessageBar = new System.Windows.Forms.Panel();
+            ((System.ComponentModel.ISupportInitialize)(this.webView)).BeginInit();
+            this.headerPanel.SuspendLayout();
+            this.infoMessageBar.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // webView
+            // 
+            this.webView.AllowExternalDrop = true;
+            this.webView.CreationProperties = null;
+            this.webView.DefaultBackgroundColor = System.Drawing.Color.White;
+            this.webView.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.webView.Location = new System.Drawing.Point(0, 186);
+            this.webView.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
+            this.webView.Name = "webView";
+            this.webView.Size = new System.Drawing.Size(2133, 1006);
+            this.webView.TabIndex = 0;
+            this.webView.ZoomFactor = 1D;
+            // 
+            // headerPanel
+            // 
+            this.headerPanel.Controls.Add(this.progressRing);
+            this.headerPanel.Controls.Add(this.btnUnlock);
+            this.headerPanel.Controls.Add(this.btnRefresh);
+            this.headerPanel.Controls.Add(this.btnGithub);
+            this.headerPanel.Controls.Add(this.titleLabel);
+            this.headerPanel.Controls.Add(this.chkAutoUnlock);
+            this.headerPanel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.headerPanel.Location = new System.Drawing.Point(0, 0);
+            this.headerPanel.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
+            this.headerPanel.Name = "headerPanel";
+            this.headerPanel.Padding = new System.Windows.Forms.Padding(27, 24, 27, 24);
+            this.headerPanel.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.headerPanel.Size = new System.Drawing.Size(2133, 112);
+            this.headerPanel.TabIndex = 1;
+            // 
+            // progressRing
+            // 
+            this.progressRing.Location = new System.Drawing.Point(1200, 24);
+            this.progressRing.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
+            this.progressRing.MarqueeAnimationSpeed = 50;
+            this.progressRing.Name = "progressRing";
+            this.progressRing.Size = new System.Drawing.Size(72, 64);
+            this.progressRing.Style = System.Windows.Forms.ProgressBarStyle.Marquee;
+            this.progressRing.TabIndex = 4;
+            this.progressRing.Visible = false;
+            // 
+            // btnUnlock
+            // 
+            this.btnUnlock.Font = new System.Drawing.Font("Segoe UI", 8.25F);
+            this.btnUnlock.Location = new System.Drawing.Point(536, 24);
+            this.btnUnlock.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
+            this.btnUnlock.MinimumSize = new System.Drawing.Size(320, 0);
+            this.btnUnlock.Name = "btnUnlock";
+            this.btnUnlock.Size = new System.Drawing.Size(400, 64);
+            this.btnUnlock.TabIndex = 3;
+            this.btnUnlock.Text = "فعال کردن دسترسی ادمین";
+            this.btnUnlock.UseVisualStyleBackColor = true;
+            this.btnUnlock.Click += new System.EventHandler(this.BtnUnlock_Click);
+            // 
+            // btnRefresh
+            // 
+            this.btnRefresh.Font = new System.Drawing.Font("Segoe UI", 8.25F);
+            this.btnRefresh.Location = new System.Drawing.Point(200, 24);
+            this.btnRefresh.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
+            this.btnRefresh.MinimumSize = new System.Drawing.Size(320, 0);
+            this.btnRefresh.Name = "btnRefresh";
+            this.btnRefresh.Size = new System.Drawing.Size(320, 64);
+            this.btnRefresh.TabIndex = 2;
+            this.btnRefresh.Text = "بارگذاری مجدد";
+            this.btnRefresh.UseVisualStyleBackColor = true;
+            this.btnRefresh.Click += new System.EventHandler(this.BtnRefresh_Click);
+            // 
+            // btnGithub
+            // 
+            this.btnGithub.Font = new System.Drawing.Font("Segoe UI", 8.25F);
+            this.btnGithub.Location = new System.Drawing.Point(32, 24);
+            this.btnGithub.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
+            this.btnGithub.Name = "btnGithub";
+            this.btnGithub.Size = new System.Drawing.Size(152, 64);
+            this.btnGithub.TabIndex = 1;
+            this.btnGithub.Text = "گیت‌هاب";
+            this.btnGithub.UseVisualStyleBackColor = true;
+            this.btnGithub.Click += new System.EventHandler(this.BtnGithub_Click);
+            // 
+            // titleLabel
+            // 
+            this.titleLabel.Dock = System.Windows.Forms.DockStyle.Right;
+            this.titleLabel.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.titleLabel.Location = new System.Drawing.Point(1349, 24);
+            this.titleLabel.Margin = new System.Windows.Forms.Padding(8, 0, 8, 0);
+            this.titleLabel.Name = "titleLabel";
+            this.titleLabel.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.titleLabel.Size = new System.Drawing.Size(757, 64);
+            this.titleLabel.TabIndex = 0;
+            this.titleLabel.Text = "مدیریت دسترسی ادمین مودم HA6400";
+            this.titleLabel.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // chkAutoUnlock
+            // 
+            this.chkAutoUnlock.Font = new System.Drawing.Font("Segoe UI", 8.25F);
+            this.chkAutoUnlock.Location = new System.Drawing.Point(952, 29);
+            this.chkAutoUnlock.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
+            this.chkAutoUnlock.Name = "chkAutoUnlock";
+            this.chkAutoUnlock.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.chkAutoUnlock.Size = new System.Drawing.Size(232, 57);
+            this.chkAutoUnlock.TabIndex = 5;
+            this.chkAutoUnlock.Text = "آنلاک خودکار";
+            this.chkAutoUnlock.CheckedChanged += new System.EventHandler(this.ChkAutoUnlock_CheckedChanged);
+            // 
+            // infoMessageText
+            // 
+            this.infoMessageText.Dock = System.Windows.Forms.DockStyle.Top;
+            this.infoMessageText.Location = new System.Drawing.Point(13, 12);
+            this.infoMessageText.Margin = new System.Windows.Forms.Padding(0);
+            this.infoMessageText.Name = "infoMessageText";
+            this.infoMessageText.Padding = new System.Windows.Forms.Padding(13, 12, 13, 12);
+            this.infoMessageText.Size = new System.Drawing.Size(2107, 55);
+            this.infoMessageText.TabIndex = 0;
+            this.infoMessageText.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // infoMessageBar
+            // 
+            this.infoMessageBar.Controls.Add(this.infoMessageText);
+            this.infoMessageBar.Dock = System.Windows.Forms.DockStyle.Top;
+            this.infoMessageBar.Location = new System.Drawing.Point(0, 112);
+            this.infoMessageBar.Margin = new System.Windows.Forms.Padding(27, 0, 27, 24);
+            this.infoMessageBar.Name = "infoMessageBar";
+            this.infoMessageBar.Padding = new System.Windows.Forms.Padding(13, 12, 13, 12);
+            this.infoMessageBar.Size = new System.Drawing.Size(2133, 74);
+            this.infoMessageBar.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(2133, 1192);
+            this.Controls.Add(this.webView);
+            this.Controls.Add(this.infoMessageBar);
+            this.Controls.Add(this.headerPanel);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
+            this.Name = "Form1";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.Text = "MCI - HA6400 Hidden Settings Unlocker";
+            ((System.ComponentModel.ISupportInitialize)(this.webView)).EndInit();
+            this.headerPanel.ResumeLayout(false);
+            this.infoMessageBar.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Microsoft.Web.WebView2.WinForms.WebView2 webView;
+        private Panel headerPanel;
+        private Label titleLabel;
+        private Button btnGithub;
+        private Button btnRefresh;
+        private Button btnUnlock;
+        private CheckBox chkAutoUnlock;
+        private ProgressBar progressRing;
+        private Label infoMessageText;
+        private Panel infoMessageBar;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,239 @@
+﻿using Microsoft.Web.WebView2.Core;
+using System;
+using System.Diagnostics;
+using System.Drawing;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HA6400AdminUnlocker
+{
+    public partial class Form1 : Form
+    {
+        private Timer autoCloseTimer;
+        private bool autoUnlockExecuted = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.WindowState = FormWindowState.Maximized;
+            InitializeAsync();
+            LoadSettings();
+        }
+
+        private async void InitializeAsync()
+        {
+            await InitializeWebView2Async();
+        }
+
+        private void LoadSettings()
+        {
+            chkAutoUnlock.Checked = Properties.Settings.Default.AutoUnlockEnabled;
+        }
+
+        private void SaveSettings()
+        {
+            Properties.Settings.Default.AutoUnlockEnabled = chkAutoUnlock.Checked;
+            Properties.Settings.Default.Save();
+        }
+
+        private async Task InitializeWebView2Async()
+        {
+            try
+            {
+                progressRing.Visible = true;
+                btnUnlock.Enabled = false;
+
+                var env = await CoreWebView2Environment.CreateAsync();
+                await webView.EnsureCoreWebView2Async(env);
+
+                webView.CoreWebView2.Settings.IsScriptEnabled = true;
+                webView.CoreWebView2.Settings.AreDevToolsEnabled = false;
+                webView.CoreWebView2.NavigationCompleted += WebView_NavigationCompleted;
+                webView.CoreWebView2.Navigate("http://192.168.1.1");
+
+                progressRing.Visible = false;
+                btnUnlock.Enabled = true;
+            }
+            catch (Exception ex)
+            {
+                ShowInfoMessage("خطا", $"خطا در راه‌اندازی مرورگر: {ex.Message}", "Error");
+            }
+        }
+
+        private async void WebView_NavigationCompleted(object sender, CoreWebView2NavigationCompletedEventArgs e)
+        {
+            if (!e.IsSuccess)
+            {
+                ShowInfoMessage("خطا", "خطا در بارگذاری صفحه مودم", "Error");
+                return;
+            }
+
+            var currentUrl = webView.Source.ToString();
+
+            if (currentUrl.EndsWith("/Login.html", StringComparison.OrdinalIgnoreCase))
+            {
+                this.Invoke((MethodInvoker)delegate
+                {
+                    btnUnlock.Text = "لطفاً ابتدا وارد شوید";
+                    btnUnlock.Enabled = false;
+                });
+
+                ShowInfoMessage("توجه", "برای استفاده از قابلیت دسترسی ادمین، ابتدا وارد حساب مدیریت مودم شوید", "Info", true);
+                return;
+            }
+
+            if (currentUrl.EndsWith("/overview.html", StringComparison.OrdinalIgnoreCase) ||
+                currentUrl.EndsWith("/index.html", StringComparison.OrdinalIgnoreCase))
+            {
+                ShowInfoMessage("توجه", "با موفقیت به صفحه‌ی مدیریت مودم وارد شده‌اید!", "Success", true);
+
+                if (chkAutoUnlock.Checked && !autoUnlockExecuted)
+                {
+                    autoUnlockExecuted = true;
+                    await BtnUnlock_ClickAsync();
+                }
+                return;
+            }
+
+            await CheckStorageStatus();
+        }
+
+        private async Task CheckStorageStatus()
+        {
+            try
+            {
+                var result = await webView.CoreWebView2.ExecuteScriptAsync(
+                    "JSON.stringify({level: localStorage.getItem('level')})");
+
+                this.Invoke((MethodInvoker)delegate
+                {
+                    btnUnlock.Text = result.Contains("\"level\":\"01\"") ?
+                        "دسترسی ادمین فعال است" : "فعال کردن دسترسی ادمین";
+                    btnUnlock.Enabled = !result.Contains("\"level\":\"01\"");
+                });
+            }
+            catch (Exception ex)
+            {
+                ShowInfoMessage("خطا", $"خطا در بررسی وضعیت ذخیره سازی: {ex.Message}", "Error");
+            }
+        }
+
+        private async void BtnUnlock_Click(object sender, EventArgs e)
+        {
+            await BtnUnlock_ClickAsync();
+        }
+
+        private async Task BtnUnlock_ClickAsync()
+        {
+            try
+            {
+                progressRing.Visible = true;
+                btnUnlock.Enabled = false;
+
+                // تغییر مقدار localStorage
+                await webView.CoreWebView2.ExecuteScriptAsync(
+                    "localStorage.setItem('level', '01');" +
+                    "localStorage.setItem('testKey', 'testValue');");
+
+                // رفرش صفحه و انتظار برای بارگذاری کامل
+                webView.CoreWebView2.Reload();
+
+                // ثبت event handler برای NavigationCompleted
+                var completionSource = new TaskCompletionSource<bool>();
+                void Handler(object s, CoreWebView2NavigationCompletedEventArgs args)
+                {
+                    webView.CoreWebView2.NavigationCompleted -= Handler;
+                    completionSource.TrySetResult(args.IsSuccess);
+                }
+
+                webView.CoreWebView2.NavigationCompleted += Handler;
+
+                // انتظار برای تکمیل بارگذاری
+                var success = await completionSource.Task;
+                if (!success)
+                {
+                    ShowInfoMessage("خطا", "خطا در بارگذاری مجدد صفحه", "Error");
+                    return;
+                }
+
+                // بررسی وضعیت جدید
+                await CheckStorageStatus();
+
+                ShowInfoMessage("موفقیت", "دسترسی ادمین با موفقیت فعال شد", "Success", true);
+            }
+            catch (Exception ex)
+            {
+                ShowInfoMessage("خطا", $"خطا در فعال کردن دسترسی ادمین: {ex.Message}", "Error");
+            }
+            finally
+            {
+                progressRing.Visible = false;
+            }
+        }
+
+        private async void BtnRefresh_Click(object sender, EventArgs e)
+        {
+            webView.Reload();
+        }
+
+        private void BtnGithub_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Process.Start("https://github.com/MEHDIMYADI/HA6400AdminUnlocker");
+            }
+            catch (Exception ex)
+            {
+                ShowInfoMessage("خطا", $"خطا در باز کردن لینک گیت‌هاب: {ex.Message}", "Error");
+            }
+        }
+
+        private void ChkAutoUnlock_CheckedChanged(object sender, EventArgs e)
+        {
+            SaveSettings();
+        }
+
+        private void ShowInfoMessage(string title, string message, string severity, bool autoClose = false, int closeAfterSeconds = 5)
+        {
+            this.Invoke((MethodInvoker)delegate
+            {
+                infoMessageText.Text = $"{title}: {message}";
+
+                // تغییر رنگ بر اساس نوع پیام
+                switch (severity)
+                {
+                    case "Error":
+                        infoMessageBar.BackColor = Color.LightPink;
+                        break;
+                    case "Success":
+                        infoMessageBar.BackColor = Color.LightGreen;
+                        break;
+                    case "Info":
+                    default:
+                        infoMessageBar.BackColor = Color.LightBlue;
+                        break;
+                }
+
+                if (autoClose)
+                {
+                    if (autoCloseTimer != null)
+                    {
+                        autoCloseTimer.Stop();
+                        autoCloseTimer = null;
+                    }
+
+                    autoCloseTimer = new Timer
+                    {
+                        Interval = closeAfterSeconds * 1000
+                    };
+                    autoCloseTimer.Tick += (s, e) =>
+                    {
+                        autoCloseTimer.Stop();
+                        autoCloseTimer = null;
+                    };
+                    autoCloseTimer.Start();
+                }
+            });
+        }
+    }
+}
